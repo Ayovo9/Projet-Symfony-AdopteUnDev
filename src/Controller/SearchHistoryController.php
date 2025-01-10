62a7006dfac3bf8d4294dca6bcecd7da917d2d80
Amevi Yovo <ayovo@univ-rennes1.fr>
2025-01-10 16:52:06 +0100

    Amélioration de la recherche, option de supprimer historique

--- src/Controller/SearchHistoryController.php
+++ src/Controller/SearchHistoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\SearchHistoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,17 +14,57 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class SearchHistoryController extends AbstractController
 {
     public function __construct(
-        private SearchHistoryRepository $searchHistoryRepository
+        private SearchHistoryRepository $searchHistoryRepository,
+        private EntityManagerInterface $entityManager
     ) {
     }
 
     #[Route('', name: 'app_search_history_index', methods: ['GET'])]
     public function index(): Response
     {
-        $searchHistory = $this->searchHistoryRepository->findUserHistory($this->getUser());
+        $searchHistory = $this->searchHistoryRepository->findBy(
+            ['user' => $this->getUser()],
+            ['searchedAt' => 'DESC']
+        );
 
         return $this->render('search_history/index.html.twig', [
             'search_history' => $searchHistory,
         ]);
+    }
+
+    #[Route('/{id}/delete', name: 'app_search_history_delete', methods: ['POST'])]
+    public function delete(int $id): Response
+    {
+        $searchHistory = $this->searchHistoryRepository->find($id);
+
+        if (!$searchHistory) {
+            throw $this->createNotFoundException('Historique non trouvé');
+        }
+
+        // Vérification de sécurité
+        if ($searchHistory->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Vous n\'êtes pas autorisé à supprimer cet historique');
+        }
+
+        $this->entityManager->remove($searchHistory);
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'Historique supprimé avec succès');
+        return $this->redirectToRoute('app_search_history_index');
+    }
+
+    #[Route('/clear', name: 'app_search_history_clear', methods: ['POST'])]
+    public function clearAll(): Response
+    {
+        $histories = $this->searchHistoryRepository->findBy(['user' => $this->getUser()]);
+        
+        foreach ($histories as $history) {
+            $this->entityManager->remove($history);
+        }
+        
+        $this->entityManager->flush();
+        
+        $this->addFlash('success', 'Historique entièrement effacé');
+        return $this->redirectToRoute('app_search_history_index');
     }
 }
